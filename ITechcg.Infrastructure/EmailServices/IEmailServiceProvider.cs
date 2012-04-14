764d41a377ee5112be252c675bc2e2bfc8533fd0
Ashim Pokharel <ashimpok@gmail.com>
2012-04-13 23:38:58 -0400

    added Smtp implementation of IEmailServiceProvider.cs

---
+++ ITechcg.Infrastructure/EmailServices/IEmailServiceProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+
+namespace ITechcg.Infrastructure.EmailServices
+{
+    /// <summary>
+    /// Interface is used by Email Processor to deal with the MailMessage after it is ready for sending.
+    /// SmtpEmailServiceProvider class implements the SMTP send thus delivering the email right away.
+    /// </summary>
+    public interface IEmailServiceProvider
+    {
+        void SendEmail(MailMessage Mail);
+    }
+}
---
+++ ITechcg.Infrastructure/EmailServices/SmtpEmailServiceProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+
+namespace ITechcg.Infrastructure.EmailServices
+{
+    /// <summary>
+    /// Email provider that uses SMTP to deliver the email
+    /// </summary>
+    public class SmtpEmailServiceProvider : IEmailServiceProvider
+    {
+        public SmtpEmailServiceProvider() { }
+
+        #region IEmailServiceProvider Members
+
+        public void SendEmail(MailMessage Message)
+        {
+            ///TODO: Relies on Smtp Setting on the config file
+            ///Remove this dependency
+            SmtpClient client = new SmtpClient();
+            client.Send(Message);
+        }
+
+        #endregion
+    }
+}
